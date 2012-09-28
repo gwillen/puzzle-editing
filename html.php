ea4cdd54091536be93f79ad5ec22f00186649f57
Glenn Willen <gwillen@nerdnet.org>
2012-09-28 03:30:53 -0500

    show testadmin testsolve request count

--- html.php
+++ html.php
@@ -159,6 +159,7 @@
                                 <?php if ($showAuthorsAndEditors) {echo '<th class="puzzidea">Editors</th>';} ?>
                                 <?php if ($showTesters) {echo '<th class="puzzidea">Testers</th>';} ?>
                                 <?php if ($showTesters) {echo '<th class="puzzidea">Last Test Report</th>';} ?>
+                                <?php if ($showTesters) {echo '<th class="puzzidea">Testsolve requests</th>';} ?>
                         </tr>
                 </thead>
                 <tbody>
@@ -213,6 +214,7 @@
                                 <?php if ($showAuthorsAndEditors) {echo "<td class='puzzidea'>" . getEditorsAsList($pid) . "</td>";} ?>
                                 <?php if ($showTesters) {echo "<td class='puzzidea'>" . getCurrentTestersAsList($pid) . "</td>";} ?>
                                 <?php if ($showTesters) {echo "<td class='puzzidea'>" .  getLastTestReportDate($pid) . "</td>";} ?>
+                                <?php if ($showTesters) {echo "<td class='puzzidea'>" .  getTestsolveRequestsForPuzzle($pid) . "</td>";} ?>
                         </tr>
 <?php
                 }
